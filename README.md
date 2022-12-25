021cb7e59c3e94ab7827239200f7ef02a292faca
HSParsian <121383214+HSParsian@users.noreply.github.com>
2022-12-25 15:46:14 +0330

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-camera-test
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-pmk1gr)
---
+++ src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  name = 'Angular ' + VERSION.major;
+  cameraOutput;
+  cameraPhoto;
+  photo;
+
+  startCamera(): void {
+    navigator.mediaDevices
+      .getUserMedia({ video: true, audio: false })
+      .then((output) => (this.cameraOutput.srcObject = output))
+      .catch((error) => console.log(error));
+  }
+
+  takePhoto(): void {
+    this.cameraPhoto
+      .getContext('2d')
+      .drawImage(this.cameraOutput, 0, 0, 300, 300);
+
+    this.photo = this.cameraPhoto.toDataURL('image/jpeg');
+  }
+}
